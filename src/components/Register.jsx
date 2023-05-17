642d0716f8013e0a5a1d683f16f995608005c3e2
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-17 13:07:34 +0600

    change some text register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Please Register!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">New to This Website Please Register</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleRegister} className="card-body">
